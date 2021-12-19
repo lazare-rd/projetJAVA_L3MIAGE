23a89a4b3d7853f303db07f99b8d2f762b970cef
Lazare Ricour-Dumas <lazare.rd@gmail.com>
2021-12-19 22:10:13 +0100

    tests JUnit #1

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+import articles.Accessoire;
+import articles.Article;
+import articles.PieceDetachee;
+import articles.Velo;
+import commandes.Client;
+import commandes.Commande;
+
 /**
  * @author Nicolas Copsidas, Leanne Robert, Lazare Ricour-Dumas
  * @version 0.1
@@ -5,8 +12,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-
-        System.out.println(ProcessFiles.databaseObjectToClient("dataClient.txt").get(0).toString());
 	}
 	
 
---
+++ tests/BoutiqueTest.java
@@ -0,0 +1,20 @@
+package tests;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Assert;
+
+import articles.Accessoire;
+import articles.Article;
+import articles.PieceDetachee;
+import articles.Velo;
+import commandes.Client;
+import commandes.Commande;
+
+public class BoutiqueTest {
+
+    @Test
+    public void createClient_returnsAClientObject(){
+        
+    }
+
+}
